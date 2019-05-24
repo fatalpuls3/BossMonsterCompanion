d21796a8d1e04e59d877f00e414975403fe5e6d1
Jason Miller <miller.jason@outlook.com>
2019-05-24 12:28:05 -0400

    Initial Commit

---
+++ Boss Monster Companion/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Boss Monster Companion
+//
+//  Created by Jason Miller on 5/24/19.
+//  Copyright © 2019 Jason Miller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
